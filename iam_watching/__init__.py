e5af9f6925bfab5c902ddfe6a010e7fadd7243f7
Dank <4129589+danktec@users.noreply.github.com>
2025-08-20 12:14:36 -0700

    add session or user auto detection

--- iam_watching/__init__.py
+++ iam_watching/__init__.py
@@ -1,8 +1,11 @@
 import boto3
+import botocore
 import json
 import time
+import botocore.client
+from botocore.exceptions import NoCredentialsError, PartialCredentialsError, ClientError
 
-__version__ = "1.3.0"
+__version__ = "1.4.0"
 VERBOSE = False
 SLEEP_SECONDS = 5
 MAX_RESULTS = 50
@@ -11,19 +14,39 @@ USER = ""
 
 def main() -> None:
 
-    client: boto3.client = boto3.client("cloudtrail")
-
+    global USER
     uniqueset: set = set()
+
+    try:
+        sts: botocore.client.STS = boto3.client("sts")
+        identity: dict = sts.get_caller_identity()
+        client: botocore.client.CloudTrail = boto3.client("cloudtrail")
+
+        auth_type: dict = identity["Arn"].split(":")[2]
+
+        if USER == "":
+            print("No user specified")
+            if auth_type == "sts":
+                USER = identity["Arn"].split("/")[2]
+                print(f"Using sts identity: {identity["Arn"]}")
+            if auth_type == "iam":
+                USER = identity["Arn"].split("/")[1]
+                print(f"Using iam identity: {identity["Arn"]}")
+
+    except NoCredentialsError:
+        print("No AWS credentials found.")
+    except PartialCredentialsError:
+        print("Incomplete AWS credentials.")
+    except ClientError as e:
+        print(f"Authentication failed: {e}")
 
     print(f"""
         Querying every {SLEEP_SECONDS}s for last {MAX_RESULTS} operations
         currently being performed by {USER}
-        Events can take up to 2 minutes to show up
-    """)
+        Events can take up to 2 minutes to show up""")
 
     print("""
-        Displaying unique actions only
-    """)
+        Displaying unique actions only""")
 
     print("""
         Hit Ctrl+C to stop watching security events
